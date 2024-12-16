0e9a039bfb8a21e073b74e2726a92644daa06eb1
julio-budal-smartfit <julio.budal@smartfit.com>
2024-12-16 14:24:00 -0300

    fix: pokemon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import CompoundCalculator from '@/components/CompoundCalculator';
+import PokemonCard from '@/components/PokemonCard';
 
 export default function Home() {
   return (
     <main className="min-h-screen p-2 bg-slate-50">
-      <CompoundCalculator />
+      <PokemonCard />
     </main>
   );
 }
